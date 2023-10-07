b32cf64fa08b877c10728664252588a8c4cbad30
yh-project <yoha6865@gmail.com>
2023-10-07 16:06:11 +0900

    actions test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>This is fourth test.</p>
+        <p>This is actions test.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
