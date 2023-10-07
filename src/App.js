b02d17b8a5538d3be0f794025182da78b6e347f9
yh-project <yoha6865@gmail.com>
2023-10-07 14:20:07 +0900

    third test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>This is second test.</p>
+        <p>This is third test.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
